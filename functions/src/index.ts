853d804d8456b37bc47b71cd304f15b6103240f0
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-02-13 22:38:45 +0300

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,12 +2,12 @@ import * as functions from "firebase-functions";
 
 // @ts-ignore
 // TypeScript can't see it is installed for some reason, but it works
-import * as corsProxy from "cors-anywhere";
+import * as corsAnywhere from "cors-anywhere";
 
-const corsAnywhere = corsProxy.createServer({
+const corsAnywhereServer = corsAnywhere.createServer({
     requireHeader: ["origin", "x-requested-with"],
 });
 
 export const cors = functions.https.onRequest((request, response) => {
-    corsAnywhere.emit("request", request, response);
+    corsAnywhereServer.emit("request", request, response);
 });
