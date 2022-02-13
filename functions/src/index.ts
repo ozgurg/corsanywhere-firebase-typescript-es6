697e0a0bdc5760f1a362dc14a11e7590d3b6f550
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-02-13 19:07:47 +0300

    Add sample usage

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,13 @@
 import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// @ts-ignore
+// TypeScript module can't see it for some reason, but it works
+import * as corsProxy from "cors-anywhere";
+
+const corsAnywhere = corsProxy.createServer({
+    requireHeader: ["origin", "x-requested-with"],
+});
+
+export const cors = functions.https.onRequest((request, response) => {
+    corsAnywhere.emit("request", request, response);
+});
