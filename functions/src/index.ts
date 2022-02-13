523a8b024b868df127e27534aa6b0a63efd224af
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-02-13 19:30:50 +0300

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from "firebase-functions";
 
 // @ts-ignore
-// TypeScript module can't see it for some reason, but it works
+// TypeScript can't see it is installed for some reason, but it works
 import * as corsProxy from "cors-anywhere";
 
 const corsAnywhere = corsProxy.createServer({
